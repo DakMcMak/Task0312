84b53ef412b860ca2145997a893181dfb8a2eb5f
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-19 23:35:31 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static int convertToSeconds(int hour){
+    return hour * 3600;
+}
+
+    public static void main(String[] args) {
+        System.out.println(convertToSeconds(1));
+        System.out.println(convertToSeconds(2));
+    }
+}
